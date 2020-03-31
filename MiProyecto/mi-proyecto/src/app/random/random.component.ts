f95a45f379353a1e9f529d7a1bbb133b94763154
Sergio Araque <sergio.araque97@gmail.com>
2020-03-31 17:25:30 +0200

    Añadido el color al número aleatorio

--- MiProyecto/mi-proyecto/src/app/random/random.component.ts
+++ MiProyecto/mi-proyecto/src/app/random/random.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomComponent implements OnInit {
 
+  // Número aleatorio
   numrandom: number = Math.floor(Math.random() * 10); // Obtenemos un número aleatorio del 1 al 10.
   
+  // Color
+  miamarillo: string = 'yellow';
+
   constructor() { }
 
   ngOnInit(): void {
