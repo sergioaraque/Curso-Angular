6046c13ec59dc965268bf1bd3e43656adf718592
Sergio Araque <sergio.araque97@gmail.com>
2020-03-31 17:09:20 +0200

    Creación de mi primer componente
    Con el comando ng generate component random

--- MiProyecto/mi-proyecto/src/app/app.module.ts
+++ MiProyecto/mi-proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EjemplocomponenteComponent } from './ejemplocomponente/ejemplocomponente.component';
+import { RandomComponent } from './random/random.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EjemplocomponenteComponent
+    EjemplocomponenteComponent,
+    RandomComponent
   ],
   imports: [
     BrowserModule,
