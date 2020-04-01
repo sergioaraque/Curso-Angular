e7be519e9ae550cb559339c9f1fbed36786ca04b
Sergio Araque <sergio.araque97@gmail.com>
2020-04-01 18:26:29 +0200

    Enrutado para crear una ventana y ver listado libros si accedemos a ella
    http://localhost:4200/listado-libros

--- MiProyecto/primeraapp/src/app/app.module.ts
+++ MiProyecto/primeraapp/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 import { LibrosComponent } from './libros/libros.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+// Constantes
+const rutas: Routes = [
+  {path: 'listado-libros', component: LibrosComponent}, // Para verlo habrá que ir a http://localhost:4200/listado-libros
+  {path: '', component: InicioComponent, pathMatch: 'full'} // Este será el componente por defecto
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SobreNosotrosComponent,
     LibrosComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
