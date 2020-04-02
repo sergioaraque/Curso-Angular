db9d5a3178e87a41cf885380ef4e40482297702c
Sergio Araque <sergio.araque97@gmail.com>
2020-04-02 17:27:03 +0200

    Escuchadores/Observadores para mostrar la información de los libros

--- MiProyecto/primeraapp/src/app/informacion/informacion.component.ts
+++ MiProyecto/primeraapp/src/app/informacion/informacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-informacion',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformacionComponent implements OnInit {
 
-  constructor() { }
+  libros: Array<object>;
+  libroId: number;
+  libroClick: any;
+
+  constructor(private rutaUsuarios:ActivatedRoute) {
+    this.libros = [
+      {id: '1', titulo: 'El nombre del viento', autor: 'Patrick Rothfus'},
+      {id: '2', titulo: 'Dime quién soy', autor: 'Julia Navarro'},
+      {id: '3', titulo: 'El día que se perdió la cordura', autor: 'Javier Castillo'}
+    ];
+   }
 
   ngOnInit(): void {
+    // Es un método asíncrono y se va a esperar a tener un resultado
+    this.rutaUsuarios.params.subscribe(params =>{ // suscribe es un Observable
+      this.libroId = params['libroId']; // Array asociativo
+      this.libroClick = this.libroBuscador();
+    });
+  }
+
+  // Método que filtra y devuelve el libro que tiene el id que le hemos mandado.
+  filtroId(libro) {
+    return libro.id == this;
+  }
+
+  // Método que buscará y devolverá el libro
+  libroBuscador(): object {
+    return this.libros.filter(this.filtroId, this.libroId) [0];
   }
 
 }
--- MiProyecto/primeraapp/src/app/libros/libros.component.ts
+++ MiProyecto/primeraapp/src/app/libros/libros.component.ts
@@ -9,14 +9,16 @@ export class LibrosComponent implements OnInit {
 
   libros: Array<object>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
     this.libros = [
       {id: '1', titulo: 'El nombre del viento', autor: 'Patrick Rothfus'},
       {id: '2', titulo: 'Dime quién soy', autor: 'Julia Navarro'},
       {id: '3', titulo: 'El día que se perdió la cordura', autor: 'Javier Castillo'}
     ];
+   }
+
+  ngOnInit(): void {
+
   }
 
 }
