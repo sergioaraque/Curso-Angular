75f715673ff4ae8de91ed087e4076d5516d05c52
Sergio Araque <sergio.araque97@gmail.com>
2020-04-02 17:04:29 +0200

    Enviamos un parámetro para que coja la información de ese libro

--- MiProyecto/primeraapp/src/app/app.module.ts
+++ MiProyecto/primeraapp/src/app/app.module.ts
@@ -8,10 +8,13 @@ import { LibrosComponent } from './libros/libros.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ErrorComponent } from './error/error.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 // Constantes
 const rutas: Routes = [
   {path: 'listado-libros', component: LibrosComponent}, // Para verlo habrá que ir a http://localhost:4200/listado-libros
+  {path: 'informacion/:libroId', component: InformacionComponent}, // :libroId es una variable
+  {path: 'informacion', redirectTo: '/'}, // Si alguien no introduce el id del libro, le redirigimos a la portada
   // Estas rutas, al ser más globales las dejamos para el final
   {path: '', component: InicioComponent, pathMatch: 'full'}, // Este será el componente por defecto
   {path: '**', component: ErrorComponent} // Si introducimos otro que no exista anteriormente, nos devuelve a la raiz.
@@ -24,7 +27,8 @@ const rutas: Routes = [
     LibrosComponent,
     CabeceraComponent,
     InicioComponent,
-    ErrorComponent
+    ErrorComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule,
--- MiProyecto/primeraapp/src/app/libros/libros.component.ts
+++ MiProyecto/primeraapp/src/app/libros/libros.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibrosComponent implements OnInit {
 
+  libros: Array<object>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.libros = [
+      {id: '1', titulo: 'El nombre del viento', autor: 'Patrick Rothfus'},
+      {id: '2', titulo: 'Dime quién soy', autor: 'Julia Navarro'},
+      {id: '3', titulo: 'El día que se perdió la cordura', autor: 'Javier Castillo'}
+    ];
   }
 
 }
