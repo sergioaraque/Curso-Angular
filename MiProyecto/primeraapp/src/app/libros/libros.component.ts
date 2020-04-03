3672de59fdefe4e2b043ec51accc38a01c65fcb0
Sergio Araque <sergio.araque97@gmail.com>
2020-04-03 16:57:22 +0200

    Loader para que se muestre cuando se están cargando los datos

--- MiProyecto/primeraapp/src/app/libros/libros.component.ts
+++ MiProyecto/primeraapp/src/app/libros/libros.component.ts
@@ -10,8 +10,8 @@ import { LibroclickedService } from '../libroclicked.service';
 export class LibrosComponent implements OnInit {
 
   libros: any;
-  errorHttp: boolean;
-  verLoader: boolean;
+  errorHttp: boolean = false; // Por defecto se establece en indefinido pero nosotros le pondremos ya el false
+  verLoader: boolean; // Nos dirá si nos está cargando los datos
 
   constructor(private http: HttpClient, public libroClicked: LibroclickedService) { // Private porque realiza la conexión y público porque habla con el servidor
 
@@ -19,17 +19,16 @@ export class LibrosComponent implements OnInit {
 
   ngOnInit(): void {
     this.verLoader = true;
-    console.log(this.verLoader);
     this.cargarLista();
-    this.verLoader = false;
-    console.log(this.verLoader);
   }
 
   // Método para cargar la lista de los libros
   cargarLista() {
     // Nos suscribimos, osea que Angular va a estar mirando a esta página
     this.http.get('assets/lista-libros.json').subscribe(
-      (respuesta: Response) => {this.libros = respuesta;}, // Obtenemos la respuesta y es cuando es okey.
+      (respuesta: Response) => {this.libros = respuesta;
+        this.verLoader = false;
+      }, // Obtenemos la respuesta y es cuando es okey.
       (respuesta: Response) => {this.errorHttp = true;}// Esto es cuando ha dado error. Si hay un error, la variable errorHttp la ponemos a true
     )
   }
