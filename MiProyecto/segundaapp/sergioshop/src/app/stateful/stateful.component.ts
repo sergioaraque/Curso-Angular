e5a154338bdfdfd528778f4e5757f312378b1a02
Sergio Araque <sergio.araque97@gmail.com>
2020-04-06 17:52:48 +0200

    componente Stateful creado

---
+++ MiProyecto/segundaapp/sergioshop/src/app/stateful/stateful.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../interface/product';
+import { Shop } from '../models/shop.model';
+
+@Component({
+  selector: 'app-stateful',
+  templateUrl: './stateful.component.html',
+  styleUrls: ['./stateful.component.css']
+})
+export class StatefulComponent implements OnInit {
+
+  // Creamos el modelo
+  shopModel: Shop = new Shop();
+
+  // Listado de elementos que se han comprado
+  boughtItems: Array<Product>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
