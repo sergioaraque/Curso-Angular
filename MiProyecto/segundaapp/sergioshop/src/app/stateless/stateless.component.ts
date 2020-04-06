7253506a06338a548b01e564cb1bd009d490af49
Sergio Araque <sergio.araque97@gmail.com>
2020-04-06 18:29:12 +0200

    Si compra un producto, que no le deje comprar otro más

--- MiProyecto/segundaapp/sergioshop/src/app/stateless/stateless.component.ts
+++ MiProyecto/segundaapp/sergioshop/src/app/stateless/stateless.component.ts
@@ -9,10 +9,25 @@ import { Product } from '../interface/product';
 export class StatelessComponent implements OnInit {
 
   @Input () product: Product; 
+  public matricula: string;
+  private disabled: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    // Damos texto al botón
+    this.matricula = 'Matricularse';
+  }
+
+  // Cuando se matriculan, se desativa el botón y se muestra que están matriculados
+  matricularse() {
+    this.disabled = true;
+    this.matricula = 'Usted está matriculado';
+  }
+
+  // Devuelve si está deshabilitado
+  isDisabled() {
+    return this.disabled;
   }
 
 }
