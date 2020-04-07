b0b9d1adc17c65c14d4a54a6c99dd76e4d097bea
Sergio Araque <sergio.araque97@gmail.com>
2020-04-07 17:36:27 +0200

    Fix nueva carpeta de sergioshop

---
+++ MiProyecto/sergioshop/src/app/confirm/confirm.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.css']
+})
+export class ConfirmComponent implements OnInit {
+
+  isDisabled: boolean;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isDisabled = false;
+  }
+
+
+}
